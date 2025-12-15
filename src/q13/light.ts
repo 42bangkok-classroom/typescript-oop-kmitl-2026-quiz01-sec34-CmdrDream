b25708a3250e93cda6781b8e13000c02c65f0d7b
CmdrDream <chayakorn11062549@gmail.com>
2025-12-15 18:08:59 +0700

    update 2

--- src/q13/light.ts
+++ src/q13/light.ts
@@ -4,6 +4,12 @@ switch (light) {
     case "red":
     console.log("Stop");
     break;
+    case "RED":
+    console.log("Stop");
+    break;
+    case "Red":
+    console.log("Stop");
+    break;
     case "yellow":
     console.log("Caution");
     break;
