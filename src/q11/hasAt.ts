d76496ea5647b789bae77226f5503891b50277d5
CmdrDream <chayakorn11062549@gmail.com>
2025-12-15 18:03:26 +0700

    update quiz

--- src/q11/hasAt.ts
+++ src/q11/hasAt.ts
@@ -1,5 +1,3 @@
 const text = process.argv[2];
 
 console.log(text.includes('@') ? "Has @" : "No @");
-
-export{};
--- src/q19/NumberRepetition.ts
+++ src/q19/NumberRepetition.ts
@@ -1,9 +1,10 @@
 const x = Number(process.argv[2]);
 
 for(let i = 0; i < x; i++){
-    let str = "";
-    for (let j = 0; j < x; j++);{
-        str += String(i+1);
-    }
-    console.log(str);
+  let str = "";
+  for (let j = 0; j < x; j++) {
+    str += String(i+1);
+  }
+  console.log(str);
+
 }
